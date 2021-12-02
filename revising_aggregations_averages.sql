0961b4f9a1bddda3044ec0502ac23632083ce311
Prashantsingh-8 <82604415+Prashantsingh-8@users.noreply.github.com>
2021-12-02 22:28:22 +0530

    .

---
+++ revising_aggregations_averages.sql
@@ -0,0 +1,21 @@
+/*
+# Revising Aggregations - Averages
+Query the average population of all cities in CITY where DISTRICT is California.
+## Input Format
+The CITY table is described as follows:
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
++-------------+--------------+
+*/
+
+sol:-
+
+SELECT AVG(POPULATION)
+FROM CITY
+WHERE DISTRICT = "California";
