630456e721189fed8215a0d206143bf5e75aad1f
Prashantsingh-8 <82604415+Prashantsingh-8@users.noreply.github.com>
2021-12-05 08:38:38 +0530

    .

---
+++ Weather Observation Station 19.sql
@@ -0,0 +1,23 @@
+--Problem Statement
+/*
+Given a table STATION that holds data for five fields namely ID, CITY, STATE, NORTHERN LATITUDE and WESTERN LONGITUDE.
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+ 
+Consider P1(a, b) and P2(c, d) be two points on 2D plane, where (a, b) be minimum and maximum values of Northern Latitude and (c, d) be minimum and maximum values of Western Longitude.
+Write a query to print the Euclidean Distance between points P1 and P2 up to 4 decimal digits.
+*/
+
+--Solution
+
+SELECT ROUND(
+         SQRT(
+           POWER(MAX(LAT_N) - MIN(LAT_N), 2) + POWER(MAX(LONG_W) - MIN(LONG_W), 2) ), 4) 
+FROM STATION;
